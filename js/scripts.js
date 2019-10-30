31a9d4f32e4d0b6e39d6db8e3912c79cab619fc2
Jordan Safford and Sharon Lee <sharon@>
2019-10-30 08:47:31 -0700

    make leapyear detector

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+var leapYear = function(year) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+
+    $(".year").text(year);
+
+    if (!result) {
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
+  });
+});
